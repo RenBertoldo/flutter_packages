fccf71d4e153d362a66d80254594a739fa6112f8
RenBertoldo <38046801+RenBertoldo@users.noreply.github.com>
2023-10-03 14:38:13 -0300

    adding export preset to remove default compressing.

--- packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
+++ packages/image_picker/image_picker_ios/ios/Classes/FLTImagePickerPlugin.m
@@ -262,6 +262,7 @@ typedef NS_ENUM(NSInteger, ImagePickerClassType) { UIImagePickerClassType, PHPic
     (NSString *)kUTTypeMovie, (NSString *)kUTTypeAVIMovie, (NSString *)kUTTypeVideo,
     (NSString *)kUTTypeMPEG4
   ];
+  imagePickerController.videoExportPreset = AVAssetExportPresetPassthrough;
   imagePickerController.videoQuality = UIImagePickerControllerQualityTypeHigh;
 
   if (maxDurationSeconds) {
